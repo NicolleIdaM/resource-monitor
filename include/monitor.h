294b741a3b888ce165e418998411f0c27e61ea50
NicolleIdaM <nicolleidamuller@gmail.com>
2025-11-11 09:14:45 -0300

    Criação das métricas do CPU, MEMÓRIA e IO.

---
+++ include/monitor.h
@@ -0,0 +1,31 @@
+#ifndef MONITOR_H
+#define MONITOR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <dirent.h>
+
+typedef struct{
+    unsigned long tempo_usuario;
+    unsigned long tempo_sistema;
+    double porcentagem_cpu;
+}metricas_cpu;
+
+typedef struct{
+    unsigned long RAM;
+    unsigned long MV;
+    unsigned long MTD;
+}metricas_memoria;
+
+typedef struct{
+    unsigned long bytes_lidos;
+    unsigned long bytes_escritos;
+    unsigned long chamadas_lidas;
+    unsigned long chamadas_escritas;
+}metricas_io;
+
+#endif
