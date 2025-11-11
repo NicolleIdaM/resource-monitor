85298b18ad70cde18ebc39dd30ae3e98b0deafd9
NicolleIdaM <nicolleidamuller@gmail.com>
2025-11-11 09:42:56 -0300

    Funções do Resource Profiler.

--- include/monitor.h
+++ include/monitor.h
@@ -13,20 +13,20 @@ typedef struct{
     unsigned long tempo_usuario;
     unsigned long tempo_sistema;
     double porcentagem_cpu;
-}metricas_cpu;
+}metricas_cpu_t;
 
 typedef struct{
     unsigned long RAM;
     unsigned long MV;
     unsigned long MTD;
-}metricas_memoria;
+}metricas_memoria_t;
 
 typedef struct{
     unsigned long bytes_lidos;
     unsigned long bytes_escritos;
     unsigned long chamadas_lidas;
     unsigned long chamadas_escritas;
-}metricas_io;
+}metricas_io_t;
 
 typedef struct{
     int pid_namespace;
@@ -35,12 +35,17 @@ typedef struct{
     int net_namespace;
     int hostname_namespace;
     int ipc_namespace;
-}metricas_namespace;
+}metricas_namespace_t;
 
 typedef struct {
     char cpu_usada[64];
     char memoria_usada[64];
     char memoria_limite[64];
-} metricas_cgroup;
+} metricas_cgroup_t;
+
+int get_metricas_cpu(pid_t pid, metricas_cpu_t* metricas);
+int get_metricas_memoria(pid_t pid, metricas_memoria_t* metricas);
+int get_metricas_io(pid_t pid, metricas_io_t* metricas);
+void monitorar_processo(pid_t pid, int intervalo, int iteracoes);
 
 #endif
