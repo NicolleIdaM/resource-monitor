5908adc19950d285d1cdf2711390c169eed43a5f
NicolleIdaM <nicolleidamuller@gmail.com>
2025-11-13 14:44:37 -0300

    Leitura e processamento de cada linha, extração de valores numéricos e armazenamento dos dados no arquivo.

--- src/io_monitor.c
+++ src/io_monitor.c
@@ -16,4 +16,20 @@ int get_metricas_io(pid_t pid, metricas_io_t *metricas)
         perror("Erro ao abrir arquivo io");
         return -1;
     }
+
+    char linha[256];
+    while(fgets(linha, sizeof(linha), arquivo)){
+        if (strstr(linha, "rchar:")){
+            sscanf(linha, "rchar: %lu", &metricas->bytes_lidos);
+        }else if (strstr(linha, "wchar:")){
+            sscanf(linha, "wchar: %lu", &metricas->bytes_escritos);}
+        else if (strstr(linha, "syscr:")){
+            sscanf(linha, "syscr: %lu", &metricas->chamadas_lidas);
+        }else if (strstr(linha, "syscw:")){
+            sscanf(linha, "syscw: %lu", &metricas->chamadas_escritas);
+        }
+    }
+
+    fclose(arquivo);
+    return 0;
 }
