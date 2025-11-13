f0a0283d21d855c72f2a6e4e9e221451fdccc9ef
NicolleIdaM <nicolleidamuller@gmail.com>
2025-11-13 14:01:42 -0300

    Fun ção para gerar carga I/O. Criação de arquivo para escrita e alocação de buffer. Preenchimento do buffer de A-Z. Escreve 500 blocos de 4KB. Leitura dos 500 arquivo de 4KB e remoção do arquivo temporário.

---
+++ tests/test_io.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include "../include/monitor.h"
+
+void gerar_carga_io()
+{
+    const char *filename = "teste_io_temporario.txt";
+    const int tamanho_bloco = 4096;
+    const int qtde_bloco = 500;
+
+    int arquivo = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    if(arquivo == -1){
+        perror("Erro ao criar arquivo de teste IO");
+        return;
+    }
+
+    char *buffer = malloc(tamanho_bloco);
+    if(buffer == 0){
+        perror("Erro ao alocar buffer");
+        close(arquivo);
+        return;
+    }
+
+    for(int i = 0; i < tamanho_bloco; i++){
+        buffer[i] = 'A' + (i % 26);
+    }
+
+    for(int i = 0; i < qtde_bloco; i++){
+        write(arquivo, buffer, tamanho_bloco);
+    }
+
+    free(buffer);
+    close(arquivo);
+
+    arquivo = open(filename, O_RDONLY);
+    buffer = malloc(tamanho_bloco);
+
+    if(arquivo != -1 && buffer != 0){
+        for(int i = 0; i < qtde_bloco; i++){
+            read(arquivo, buffer, tamanho_bloco);
+        }
+        free(buffer);
+        close(arquivo);
+    }
+    unlink(filename);
+}
