6f5d4456e7d82c7acec37765296b3ae04f5862d1
NicolleIdaM <nicolleidamuller@gmail.com>
2025-11-13 12:48:26 -0300

    Criação da validação inicial do ponteiro metricas. Verificação se o arquivo foi aberto corretamente. Declaração das variáveis, leitura das variáveis e somatória total dos tempos para obter tempo total do sistema.

--- src/cpu_monitor.c
+++ src/cpu_monitor.c
@@ -7,3 +7,21 @@ static struct{
     unsigned long ultimo_tempo_processo;
     int primeira_chamada;
 }estado_cpu = {0, 0, 1};
+
+int get_metricas_cpu(pid_t pid, metricas_cpu_t* metricas){
+    if(metricas == 0){
+        return -1;
+    }
+
+    FILE *arquivo = fopen("/proc/diskstats", "r");
+    if(arquivo == 0){
+        return -1;
+    }
+    
+    unsigned long user, nice, system, idle, iowait, irq, softirq, steal, guest;
+    
+    fscanf(arquivo, "cpu %lu %lu %lu %lu %lu %lu %lu %lu %lu", &user, &nice, &system, &idle, &iowait, &irq, &softirq, &steal, &guest);
+    fclose(arquivo);
+
+    unsigned long tempo_total = user + nice + system + idle + iowait + irq + softirq + steal + guest;
+}
