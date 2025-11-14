23c290b9e1cb43fd172595a180b4077127b5461b
NicolleIdaM <nicolleidamuller@gmail.com>
2025-11-14 01:46:18 -0300

    Implementação da versão cgroup v2.

--- include/cgroup.h
+++ include/cgroup.h
@@ -3,18 +3,26 @@
 
 #include <sys/types.h>
 
-typedef struct{
+typedef struct {
     char cpu_usada[64];
     char memoria_usada[64];
     char memoria_limite[64];
-}metricas_cgroup_t;
+    char cgroup_version[16];
+} metricas_cgroup_t;
 
 int get_metricas_cgroup(pid_t pid, metricas_cgroup_t* metricas);
 int criar_cgroup(const char* nome_cgroup);
 int mover_cgroup(const char* nome_cgroup, pid_t pid);
-int limite_cpu(const char* nome_cgroup, double cpu_cores);
-int limite_memoria(const char* nome_cgroup, unsigned long memoria_mb);
 int remover_cgroup(const char* nome_cgroup);
 void listar_cgroups();
+
+int limite_cpu(const char* nome_cgroup, double cpu_cores);
+int limite_memoria(const char* nome_cgroup, unsigned long memoria_mb);
+int limite_io(const char* nome_cgroup, unsigned long ler_bytes_segudos, unsigned long escrever_bytes_segundo);
+
+int detectar_cgroup_version(void);
+int criar_cgroup_v2(const char* nome_cgroup);
+int limite_cpu_v2(const char* nome_cgroup, double cpu_cores);
+int limite_memoria_v2(const char* nome_cgroup, unsigned long memoria_mb);
 
 #endif
